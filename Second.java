87057a121386b5cb601be8f1b080fed4deb0a791
Vikash  ROY <128238949+vikashbharadwaj@users.noreply.github.com>
2025-02-05 00:57:21 +0530

    Update Second.java

--- Second.java
+++ Second.java
@@ -2,5 +2,6 @@ public class Second {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hiiii");
+        System.out.println("last);
     }
 }
