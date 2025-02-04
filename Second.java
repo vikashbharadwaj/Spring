35c7296dc505b430bd208b4021817d725f59e505
vikashbharadwaj <vikashkumariota@gmail.com>
2025-02-05 00:55:23 +0530

    this is second comment

--- Second.java
+++ Second.java
@@ -2,6 +2,5 @@ public class Second {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hiiii");
-        System.out.println("last);
     }
 }
--- first.java
+++ first.java
@@ -1,7 +1,6 @@
 public class first {
     public static void main(String[]args){
         System.out.println("hello");
-        System.out.println("first);
     }
 
 }
