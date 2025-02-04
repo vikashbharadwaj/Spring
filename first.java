f149ea6abc64f17b1f0ed2c8620a50162c7c1cef
vikashbharadwaj <vikashkumariota@gmail.com>
2025-02-05 01:11:27 +0530

    last

--- first.java
+++ first.java
@@ -1,7 +1,7 @@
 public class first {
     public static void main(String[]args){
         System.out.println("hello");
-        System.out.println("first remote);
+        System.out.println("local change");
     }
 
 }
